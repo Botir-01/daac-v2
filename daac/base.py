2af511f2bea17c4462d700f6a6eb10820edab444
Botir-01 <xasanovich01@icloud.com>
2022-04-11 16:32:12 +0500

    Removed some

--- daac/base.py
+++ daac/base.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'middleware.default_language.CustomLocaleMiddleware'
+    'middleware.default_language.CustomLocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'daac.urls'
